d7b205619a613f3475c8508667e29dc41d621ce6
vcmurali <vcmurali@hotmail.com>
2022-07-01 18:12:54 -0500

    3rd commit

---
+++ variousConcept/GitTest_4.java
@@ -0,0 +1,25 @@
+package variousConcept;
+
+public class GitTest_4 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Git Test4");
+		
+		printMe();
+		printMe1();
+		printMe2();
+	}
+	
+	public static void printMe() {
+		System.out.println("Git Test4_PrintMe");
+	}
+	
+	public static void printMe1() {
+		System.out.println("Git Test4_PrintMe 1");
+	}
+
+	public static void printMe2() {
+		System.out.println("Git Test4_PrintMe 2");
+	}
+}
